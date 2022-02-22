68375c6d7e550194130a51a5b0c6a1651f83f01e
Sebastian Heid <sebastian.heid45@gmail.com>
2022-02-22 19:00:17 +0100

    chore: update config files & fix brew updater

--- auto-update/brew/brew-auto-update.sh
+++ auto-update/brew/brew-auto-update.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo -e "\n`date`: RUNNING: brew update"
-/usr/local/bin/brew update
+/opt/homebrew/bin/brew update
 echo "`date`: FINISHED: brew update"
 
 echo -e "\n`date`: RUNNING: brew upgrade"
-/usr/local/bin/brew upgrade
+/opt/homebrew/bin/brew upgrade
 echo "`date`: FINISHED: brew upgrade"
 
 echo -e "\n`date`: RUNNING: brew cleanup"
-/usr/local/bin/brew cleanup
+/opt/homebrew/bin/brew cleanup
 echo "`date`: FINISHED: brew cleanup"
 
 echo -e "\nAll done! Enjoy a cold one! 🍺 \n"
