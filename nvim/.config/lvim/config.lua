54400d6d0e571698879f6267f40a95a9f7d81b51
Sebastian Heid <8442432+s4heid@users.noreply.github.com>
2025-01-27 13:54:21 +0100

    chore: update config

--- nvim/.config/lvim/config.lua
+++ nvim/.config/lvim/config.lua
@@ -3,18 +3,30 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 lvim.plugins = {
-    { "lunarvim/colorschemes" },
-    {
-      "stevearc/dressing.nvim",
-      config = function()
-        require("dressing").setup({
-          input = { enabled = false },
-        })
-      end,
-    },
-    {
-      "nvim-neorg/neorg",
-      ft = "norg", -- lazy-load on filetype
-      config = true, -- run require("neorg").setup()
-    },
-  }
+  { "lunarvim/colorschemes" },
+  {
+    "stevearc/dressing.nvim",
+    config = function()
+      require("dressing").setup({
+        input = { enabled = false },
+      })
+    end,
+  },
+  {
+    "ellisonleao/glow.nvim",
+    config = function()
+      require("glow").setup({
+        glow_path = "/opt/homebrew/bin/glow",
+      })
+    end,
+    ft = {"markdown"},
+  },
+  {
+    "github/copilot.vim",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
+  },
+}
